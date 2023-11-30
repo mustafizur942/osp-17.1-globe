d945d20e9189bca7d8489125f0ad3906c1340959
mustafizur <mustafizr1122@gamil.com>
2023-11-30 12:40:28 +0530

    added file

---
+++ overcloud-deployment.sh
@@ -0,0 +1,12 @@
+openstack overcloud deploy --templates \
+  -n /home/stack/templates/network_data.yaml \
+  -e /home/stack/templates/overcloud-baremetal-deployed.yaml \
+  -e /home/stack/templates/overcloud-networks-deployed.yaml \
+  -e /home/stack/templates/overcloud-vip-deployed.yaml \
+  -e /home/stack/containers-prepare-parameter.yaml \
+  -e /home/stack/inject-trust-anchor-hiera.yaml \
+  -e /home/stack/templates/disable_tsx.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/external-ceph.yaml \
+  -e /home/stack/templates/ceph-config.yaml \
+  -r /home/stack/templates/roles_data.yaml \
+  --ntp-server 172.17.1.23 
